e516696c66220b2522e914178f023d9661228ad5
Yafet Tegbaru <tegbaruyafet18@gmail.com>
2026-05-04 22:10:34 -0400

    Added main components.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Experience from './pages/Experience'
+import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/"            element={<Home />} />
+      <Route path="/about"       element={<About />} />
+      <Route path="/projects"    element={<Projects />} />
+      <Route path="/experience"  element={<Experience />} />
+      <Route path="/contact"     element={<Contact />} />
+      <Route path="*"            element={<NotFound />} />
+    </Routes>
+  )
+}
