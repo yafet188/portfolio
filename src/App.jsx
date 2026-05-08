595d9600ff867deb66d23a9319343a24e6351c96
Yafet Tegbaru <tegbaruyafet18@gmail.com>
2026-05-07 23:30:59 -0400

    Added App JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
+import ProjectDetail from './pages/ProjectDetail'
 import Experience from './pages/Experience'
 import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
@@ -9,12 +10,13 @@ import NotFound from './pages/NotFound'
 export default function App() {
   return (
     <Routes>
-      <Route path="/"            element={<Home />} />
-      <Route path="/about"       element={<About />} />
-      <Route path="/projects"    element={<Projects />} />
-      <Route path="/experience"  element={<Experience />} />
-      <Route path="/contact"     element={<Contact />} />
-      <Route path="*"            element={<NotFound />} />
+      <Route path="/"                  element={<Home />} />
+      <Route path="/about"             element={<About />} />
+      <Route path="/projects"          element={<Projects />} />
+      <Route path="/projects/:slug"    element={<ProjectDetail />} />
+      <Route path="/experience"        element={<Experience />} />
+      <Route path="/contact"           element={<Contact />} />
+      <Route path="*"                  element={<NotFound />} />
     </Routes>
   )
 }
